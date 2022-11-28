0591523a1ff038e7143cdc070f0a843e51bec79c
Seagull2004 <91527519+Seagull2004@users.noreply.github.com>
2022-11-28 10:25:10 +0100

    preloader e montserrat

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'mont' : ['Montserrat', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: [],
+} 
